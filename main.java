a973543436f5a225bdec965a5850e81c8f25c64a
sahanyarar <yararsahan@gmail.com>
2024-01-05 20:38:03 +0300

    cleared

--- main.java
+++ main.java
@@ -2,7 +2,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
-// reader-priority this means if someone is reading lock the writer
 public class Main {
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newCachedThreadPool();
